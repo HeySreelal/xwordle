9a6ba619bbdded9388e91caea0b11dc4e2eb80c7
Sreelal TS <heysreelal@gmail.com>
2023-07-16 15:52:00 +0530

    Minor change

--- lib/handlers/error.dart
+++ lib/handlers/error.dart
@@ -5,7 +5,7 @@ Future<void> errorHandler(Object err, StackTrace stack) async {
   try {
     await bot.api.sendMessage(
       WordleConfig.instance.logsChannel,
-      "$err\n\n$stack",
+      "💀 $err\n\n$stack\n\n#error",
     );
   } catch (err, stack) {
     print("Error while sending error to logs channel: $err");
