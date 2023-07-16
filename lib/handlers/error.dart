292627eb5ca120a020f5589de09268747073ef69
Sreelal TS <heysreelal@gmail.com>
2023-07-16 15:50:23 +0530

    Error log to logsChannel 👀

--- lib/handlers/error.dart
+++ lib/handlers/error.dart
@@ -1,5 +1,14 @@
-Future<void> errorHandler(Object err, StackTrace stack) {
-  print('Caught error: $err');
-  print(stack);
-  return Future.value();
+import 'package:xwordle/config/config.dart';
+import 'package:xwordle/xwordle.dart';
+
+Future<void> errorHandler(Object err, StackTrace stack) async {
+  try {
+    await bot.api.sendMessage(
+      WordleConfig.instance.logsChannel,
+      "$err\n\n$stack",
+    );
+  } catch (err, stack) {
+    print("Error while sending error to logs channel: $err");
+    print(stack);
+  }
 }
