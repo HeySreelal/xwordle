5dc3bdcd04612ce531827d14c1e05eb8c6c04beb
Sreelal TS <heysreelal@gmail.com>
2023-07-19 07:47:41 +0530

    Handle bad gateway errors 😇

--- lib/handlers/error.dart
+++ lib/handlers/error.dart
@@ -1,3 +1,5 @@
+import 'package:televerse/telegram.dart';
+import 'package:televerse/televerse.dart';
 import 'package:xwordle/config/config.dart';
 import 'package:xwordle/utils/utils.dart';
 
@@ -6,6 +8,25 @@ Future<void> errorHandler(Object err, StackTrace stack) async {
     if (WordleConfig.isDebug) {
       print("Error: $err");
       print(stack);
+      return;
+    }
+    if (err is TelegramException && err.isServerExeption) {
+      DateTime now = DateTime.now();
+      Message? msg;
+      while (msg == null) {
+        try {
+          msg = await sendLogs(
+            "⚠️ <b>Server Error</b>\n\n"
+            "<b>Time:</b> ${now.toIso8601String()}\n"
+            "<b>Message:</b> $err\n"
+            "<b>Stack:</b> $stack\n\n"
+            "#error",
+          );
+        } catch (err) {
+          print("Error ignored: $err");
+          await Future.delayed(Duration(minutes: 30));
+        }
+      }
       return;
     }
     await sendLogs("💀 $err\n\n$stack\n\n#error");
