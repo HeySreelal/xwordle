ab1e7eba595c8d5bd05fe04440c124823c1f26f9
Sreelal TS <heysreelal@gmail.com>
2024-07-04 19:21:49 +0530

    🎭 Updated broadcast logic

--- extras/broadcast.dart
+++ extras/broadcast.dart
@@ -1,11 +1,8 @@
 import 'dart:io';
-import 'package:televerse/telegram.dart' hide File;
 import 'package:televerse/televerse.dart';
-import 'package:xwordle/xwordle.dart';
+import 'package:xwordle/xwordle.dart' hide bot;
 
-void main(List<String> args) async {
-  await init();
-  final text = """
+final text = """
 Hey Wordler! This message might come as a surprise, but unexpected things happen, right? 😊
 
 Great news: the Wordle Bot is back and available for you to play! 🎉 We know you might have missed the bot and the fun of guessing the word of the day. We apologize for the sudden downtime.
@@ -14,43 +11,81 @@ However, we’re facing some challenges with covering the server costs to keep t
 
 We’ve added the /donate command to make it easy for you to support us. Every little bit helps.
 
-We’d also love to know if you enjoy playing the Wordle game. Would you mind letting us know by voting in the linked poll?
+Also also, ff there's anything on your mind, please feel free to let us know. We truly value your feedback! Use the /feedback command to tell us what you think and how we can improve your Wordle experience.
 
-If there's anything on your mind, please feel free to let us know. We truly value your feedback! Use the /feedback command to tell us what you think and how we can improve your Wordle experience.
+And as always, consider joining @Xooniverse channel where we are likely to post any updates about the Wordle bot and other projects. 
 
 Thank you for being part of our Wordle community. Enjoy the game, and let’s keep the fun going! ✨
 """;
 
-  final keyboard = InlineKeyboard()
-      .addUrl("Xooniverse Post 💬", "https://t.me/xooniverse/5");
+final api = RawAPI(WordleConfig.env["TOKEN"]);
 
-  await postToXooniverse(text, keyboard);
+void main(List<String> args) async {
+  await broadcastLogic();
 }
 
-Future<void> testBroadcast(String text) async {
-  bot.api.sendMessage(ChatID(1726826785), text);
+/// Keyboard to be attached with the broadcast message
+final keyboard =
+    InlineKeyboard().addUrl("Open @Xooniverse 🪐", "https://t.me/xooniverse");
+
+/// Sends the broadcast message to specified users
+Future<void> testBroadcast() async {
+  final chats = [
+    ChatID(1726826785),
+  ];
+  for (int i = 0; i < chats.length; i++) {
+    await api.sendMessage(
+      chats[i],
+      text,
+      replyMarkup: keyboard,
+    );
+    await Future.delayed(Duration(seconds: 1));
+  }
 }
 
-Future<void> postToXooniverse(String text, ReplyMarkup markup) async {
-  await bot.api.sendMessage(
+/// Posts the broadcast message to Xooniverse channel.
+Future<void> postToXooniverse() async {
+  await api.sendMessage(
     ChannelID("@xooniverse"),
     text,
-    replyMarkup: markup,
+    replyMarkup: keyboard,
   );
 }
 
-Future<void> broadcastLogic(String text, ReplyMarkup markup) async {
+/// Reference to the log file
+final file = File("log.log");
+
+/// Write log to file
+void addlog(String text) async {
+  // Get the current timestamp
+  String timestamp = DateTime.now().toIso8601String();
+
+  // Combine the timestamp with the log text
+  String logEntry = '$timestamp - $text\n';
+  print(logEntry);
+
+  // Append the log entry to the file
+  await file.writeAsString(logEntry, mode: FileMode.append);
+}
+
+/// Actual broadcast logic
+Future<void> broadcastLogic() async {
+  addlog("ℹ️ Starting up");
   final users = await WordleDB.getUsers();
   final ids = users.map((e) => ChatID(e.id)).toList();
 
   int count = ids.length;
-  int sent = 0, failed = 0;
+  int sent = 293, failed = 198;
   List<ErrorUser> errorUsers = [];
-  Message? statusMessage = await sendLogs(progressMessage(count, 0, 0));
+  final statusMessage = await sendLogs(progressMessage(count, sent, failed));
 
-  for (int i = 0; i < count; i++) {
+  addlog("ℹ️ Got total of $count users");
+
+  for (int i = 492; i < count; i++) {
+    addlog("  > Processing ${i + 1}: ${ids[i].id}");
     try {
-      await bot.api.sendMessage(ids[i], text, replyMarkup: markup);
+      await api.sendMessage(ids[i], text, replyMarkup: keyboard);
+      addlog("  > ✅ Sent broadcast message");
       sent++;
       await Future.delayed(Duration(milliseconds: 2000));
 
@@ -60,19 +95,30 @@ Future<void> broadcastLogic(String text, ReplyMarkup markup) async {
             statusMessage.messageId,
             progressMessage(count, sent, failed),
           );
+          addlog("📖 Updated channel log message");
           await Future.delayed(Duration(milliseconds: 2000));
         }
       }
     } catch (e) {
-      print("Failed to send message to ${ids[i].id}");
+      addlog("  > ❌ Failed to send message to ${ids[i].id}");
+      addlog("  > Error:");
+      addlog("  > $e");
       failed++;
       errorUsers.add(
         ErrorUser(ids[i].id, e.toString()),
       );
-      WordleUser user = await WordleUser.init(ids[i].id);
-      user.optedOutOfBroadcast = true;
-      user.id = ids[i].id;
-      user.save();
+      try {
+        WordleUser user = await WordleUser.init(ids[i].id);
+        user.optedOutOfBroadcast = true;
+        user.id = ids[i].id;
+        user.save().ignore();
+        addlog("  > ℹ️ Updated user's `optedOutOfBroadcast`");
+      } catch (err) {
+        addlog(
+          ">  🚫 Error occurred while trying to get user info from Firestore.",
+        );
+        addlog("    > $err");
+      }
     }
   }
 
@@ -87,7 +133,7 @@ Future<void> broadcastLogic(String text, ReplyMarkup markup) async {
       errorUsers.map((e) => e.line).join("\n"),
     );
     if (errorUsers.isNotEmpty) {
-      await bot.api.sendDocument(
+      await api.sendDocument(
         WordleConfig.instance.logsChannel,
         InputFile.fromFile(f),
         caption: "Failed IDs",
@@ -97,12 +143,14 @@ Future<void> broadcastLogic(String text, ReplyMarkup markup) async {
       try {
         f.deleteSync();
       } catch (e) {
-        print("Failed to delete file");
+        addlog("Failed to delete file");
       }
     });
   } catch (e, s) {
-    print("Failed to send logs");
-    print(e);
-    print(s);
+    addlog("Failed to send logs");
+    addlog("$e");
+    addlog("$s");
   }
+
+  addlog("ℹ️ Finished the process.");
 }
