de2ebaa20a5668b46db67c9a4d914f2fc144ef4e
Sreelal TS <heysreelal@gmail.com>
2024-07-06 20:55:19 +0530

    ℹ️ Added invite info

--- lib/handlers/hints.dart
+++ lib/handlers/hints.dart
@@ -39,6 +39,14 @@ Future<void> sendHintInfo(Context ctx, WordleUser user, bool showNewTag) async {
     (showNewTag ? "🆕 " : "") + getHintInfoMessage(user.hints),
     replyMarkup: getMoreBoard,
   );
+
+  final String referralInfo = """
+👫 You can also earn free hints by inviting friends! We offer exciting hint packs as rewards when you reach referral milestones.
+
+Send /invite to get your referral link.
+""";
+
+  await ctx.reply(referralInfo);
 }
 
 Future<void> sendHintUsage(
