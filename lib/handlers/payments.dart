72533ea64ca8aa8f18f29a175b2c91495576be9e
Sreelal TS <heysreelal@gmail.com>
2024-08-01 12:39:46 +0530

    typo

--- lib/handlers/payments.dart
+++ lib/handlers/payments.dart
@@ -2,7 +2,7 @@ part of '../xwordle.dart';
 
 Handler preCheckoutHandler() {
   return (ctx) async {
-    await ctx.answerPreCheckouQUery(true);
+    await ctx.answerPreCheckouQuery(true);
     sendLogs(
       'Precheckout Query Received\n\n<pre><code class="language-json">${ctx.preCheckoutQuery?.toJson()}</code></pre>',
     ).ignore();
